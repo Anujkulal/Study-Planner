0a17616625690116c0615ccb8411afb458bf3390
anujkulal <anujkulal333@gmail.com>
2025-11-10 17:20:16 +0530

    added redux store

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,51 @@
 import { useState, useEffect } from "react";
 import { type StudySession } from "./types";
-import { sessionsAPI } from "./api/sessions";
 import { WeeklyCalendar } from "./components/WeeklyCalendar";
 import { ProgressDashboard } from "./components/ProgressDashboard";
 import { SessionDialog } from "./components/SessionDialog";
 import { Toaster } from "sonner";
 import { Header } from "./components/Header";
+import { useDispatch, useSelector } from "react-redux";
+import type { AppDispatch, RootState } from "./redux/store";
+import { clearMessages, deleteSession, getAllSessions } from "./redux/slices/sessionSlice";
 
 const App = () => {
-  const [sessions, setSessions] = useState<StudySession[]>([]);
-  const [loading, setLoading] = useState(true);
+  // const [sessions, setSessions] = useState<StudySession[]>([]);
+  // const [loading, setLoading] = useState(true);
   const [dialogOpen, setDialogOpen] = useState(false);
   const [editingSession, setEditingSession] = useState<StudySession | null>(null);
   const [selectedDay, setSelectedDay] = useState<string | null>(null);
 
-  const loadSessions = async () => {
-    setLoading(true);
-    try {
-      const data = await sessionsAPI.getAllSessions();
-      setSessions(data);
-    } catch (error) {
-      console.error('Failed to load sessions:', error);
-    } finally {
-      setLoading(false);
-    }
-  };
+  const dispatch = useDispatch<AppDispatch>();
+  const { loading, sessions } = useSelector((state: RootState) => state.session);
 
   useEffect(() => {
-    // console.log("Min Date: ", maxDate)
-    loadSessions();
-  }, []);
+    dispatch(getAllSessions());
+  }, [dispatch])
+
+  useEffect(() => {
+    if(!dialogOpen){
+      dispatch(clearMessages())
+    }
+  }, [dialogOpen, dispatch])
+
+  // const loadSessions = async () => {
+  //   // setLoading(true);
+  //   try {
+  //     // const data = await sessionsAPI.getAllSessions();
+  //      const result = await dispatch(getAllSessions()).unwrap();
+  //     // setSessions(data);
+  //   } catch (error) {
+  //     console.error('Failed to load sessions:', error);
+  //   } finally {
+  //     // setLoading(false);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   // console.log("Min Date: ", maxDate)
+  //   loadSessions();
+  // }, []);
 
   const handleAddSession = (day?: string) => {
     setSelectedDay(day || null);
@@ -49,21 +65,27 @@ const App = () => {
       const confirmed = window.confirm('Are you sure you want to delete this session?');
       if (!confirmed) return;
 
-      await sessionsAPI.deleteSession(id);
-      await loadSessions();
+      // await sessionsAPI.deleteSession(id);
+      await dispatch(deleteSession(id)).unwrap();
+      // await loadSessions();
+      dispatch(getAllSessions());
     } catch (error) {
       console.error('Failed to delete session:', error);
     }
   };
 
    const handleSessionUpdated = async () => {
-    await loadSessions();
+    // await loadSessions();
+    dispatch(getAllSessions());
     setDialogOpen(false);
   };
 
   const handleSaveSession = async () => {
-    await loadSessions();
+    // await loadSessions();
+    dispatch(getAllSessions());
     setDialogOpen(false);
+    setEditingSession(null);
+    setSelectedDay(null);
   };
 
   return (
