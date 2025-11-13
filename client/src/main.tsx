1e054de8fb3872a64758a97ee3ebc76ab846ced6
anujkulal <anujkulal333@gmail.com>
2025-11-13 08:49:24 +0530

    updated main.tsx on 13-11-2025 08:49:24

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,12 +5,19 @@ import App from './App.tsx'
 import { ThemeProvider } from './contexts/ThemeContext.tsx'
 import { Provider } from 'react-redux'
 import { store } from './redux/store.ts'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { LandingPage } from './pages/LandingPage.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
       <Provider store={store}>
-        <App />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/app" element={<App />} />
+          </Routes>
+        </BrowserRouter>
       </Provider>
     </ThemeProvider>
   </StrictMode>,
